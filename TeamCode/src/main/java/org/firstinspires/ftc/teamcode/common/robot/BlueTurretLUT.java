bcf4a65e2ab235b4cc679b38340e980a6938fcc4
Suchir Ryali <ryalisuchir2.0@gmail.com>
2025-11-28 23:28:39 -0600

    turret works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/BlueTurretLUT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/BlueTurretLUT.java
@@ -6,11 +6,23 @@ public class BlueTurretLUT {
     private final InterpolatedLUT turretServoLUT = new InterpolatedLUT();
 
     public BlueTurretLUT() {
-        turretServoLUT.addPoint(0, 0.35); //radians, servo position
-        turretServoLUT.addPoint(45, 0.47);
-        turretServoLUT.addPoint(90, 0.60);
-        turretServoLUT.addPoint(135, 0.73);
-        turretServoLUT.addPoint(180, 0.85);
+        turretServoLUT.addPoint(-0.052053, 0.43); //radians, servo position
+        turretServoLUT.addPoint(0.065504, 0.48); //radians, servo position
+        turretServoLUT.addPoint(-0.448756, 0.35); //radians, servo position
+        turretServoLUT.addPoint(-1.267128, 0.2); //radians, servo position
+        turretServoLUT.addPoint(-1.351721, 0.15); //radians, servo position
+        turretServoLUT.addPoint(-1.746817, 0.1); //radians, servo position
+        turretServoLUT.addPoint(-2.534844, 0); //radians, servo position
+        turretServoLUT.addPoint(0.236435, 0.5); //radians, servo position
+        turretServoLUT.addPoint(0.582613, 0.55); //radians, servo position
+        turretServoLUT.addPoint(0.865427, 0.6); //radians, servo position
+        turretServoLUT.addPoint(1.147426, 0.7); //radians, servo position
+        turretServoLUT.addPoint(1.679656, 0.75); //radians, servo position
+        turretServoLUT.addPoint(1.264063, 0.8); //radians, servo position
+        turretServoLUT.addPoint(2.352483, 0.9); //radians, servo position
+        turretServoLUT.addPoint(2.326805, 0.95); //radians, servo position
+        turretServoLUT.addPoint(2.591857, 1); //radians, servo position
+
     }
 
     public double getServoValue(double robotAngle) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/ShooterLUT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/ShooterLUT.java
@@ -7,12 +7,12 @@ public class ShooterLUT {
     private final InterpolatedLUT1D shooterLUT = new InterpolatedLUT1D();
 
     public ShooterLUT() {
-        shooterLUT.addPoint(24, new ShooterParams(0.35, 2200));
-        shooterLUT.addPoint(24, new ShooterParams(0.38, 2250));
-        shooterLUT.addPoint(36, new ShooterParams(0.45, 2400));
-        shooterLUT.addPoint(36, new ShooterParams(0.48, 2450));
-        shooterLUT.addPoint(48, new ShooterParams(0.55, 2600));
-        shooterLUT.addPoint(48, new ShooterParams(0.58, 2650));
+        shooterLUT.addPoint(24, new ShooterParams(0.8, 1900));
+        shooterLUT.addPoint(24, new ShooterParams(0.8, 1900));
+        shooterLUT.addPoint(36, new ShooterParams(0.8, 1900));
+        shooterLUT.addPoint(36, new ShooterParams(0.8, 1900));
+        shooterLUT.addPoint(48, new ShooterParams(0.8, 1900));
+        shooterLUT.addPoint(48, new ShooterParams(0.8, 1900));
     }
 
     public ShooterParams getShooterValue(double distance) {
