262a1c17b201384557bc666f6cf386e0db3d3331
Suchir Ryali <ryalisuchir2.0@gmail.com>
2025-11-28 23:29:07 -0600

    added autosort commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/SubsystemTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/SubsystemTest.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.opmode.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.CommandScheduler;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+import com.seattlesolvers.solverslib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.common.commandBase.commands.ShootingMaster;
 import org.firstinspires.ftc.teamcode.common.commandBase.commands.StartIntake;
@@ -27,6 +31,7 @@ public class SubsystemTest extends CommandOpMode {
 
     @Override
     public void run() {
+        CommandScheduler.getInstance().run();
         robot.follower.setTeleOpDrive(
                 -0.5 * Math.tan(1.12 * gamepad1.left_stick_y),
                 -0.5 * Math.tan(1.12 * gamepad1.left_stick_x),
@@ -43,14 +48,34 @@ public class SubsystemTest extends CommandOpMode {
             schedule(new StartIntake());
         }
 
-        if (gamepad1.right_bumper) {
-            schedule(new ShootingMaster(robot.shooterSubsystem));
+        if (gamepad1.triangle) {
+            schedule(
+                    new SequentialCommandGroup(
+                            new InstantCommand(() -> Globals.kicker1State = Globals.Kicker1State.KICK),
+                            new WaitCommand(1000),
+                            new InstantCommand(() -> Globals.kicker2State = Globals.Kicker2State.KICK),
+                            new WaitCommand(1000),
+                            new InstantCommand(() -> Globals.kicker3State = Globals.Kicker3State.KICK),
+                            new WaitCommand(1000)
+                    )
+            );
         }
 
-        telemetry.update();
-        if (isAutomated) {
-            robot.loop(robot);
+        telemetry.addData("Kicker 1: ", Globals.kicker1State);
+        telemetry.addData("Kicker 2: ", Globals.kicker2State);
+        telemetry.addData("Kicker 3: ", Globals.kicker3State);
+
+        if (gamepad1.right_bumper) {
+            telemetry.addLine("RB Pressed");
+            schedule(new ShootingMaster());
         }
+        telemetry.addData("Turret pos:", robot.turret1.getPosition());
+        telemetry.addData("1:", Globals.ballColor1);
+        telemetry.addData("2:", Globals.ballColor2);
+        telemetry.addData("3:", Globals.ballColor3);
+
+        telemetry.update();
+        robot.loop(robot);
 
     }
 }
