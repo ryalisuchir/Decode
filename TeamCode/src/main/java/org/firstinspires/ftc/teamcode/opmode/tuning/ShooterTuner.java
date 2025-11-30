b521e0eedd7d35ef369e3acc0df8c0420874a9c5
Suchir Ryali <ryalisuchir2.0@gmail.com>
2025-11-30 13:02:26 -0600

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/ShooterTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/ShooterTuner.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmode.tuning;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.seattlesolvers.solverslib.controller.PIDFController;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.common.robot.Globals;
@@ -13,45 +16,58 @@ import org.firstinspires.ftc.teamcode.common.robot.Robot;
 @Config
 public class ShooterTuner extends OpMode {
     Robot robot;
-    static double setVelocity = 0;
     static double hoodPosition = Globals.HOOD_LOWERED;
 
-    public static double P = 0.0001;
-    public static double I = 0.0;
-    public static double D = 0.0;
-    public static double F = 0.0008;
+    public static double setPoint = 0;
 
-    public PIDFController controller;
+    private com.pedropathing.control.PIDFController b, s;
 
-    public double power;
+    public static double bp = 0.007, bd = 0.0, bf = 0.0, sp = 0.005, sd = 0, sf = 0.0;
+    public static double pSwitch = 150;
 
     @Override
     public void init() {
         robot = new Robot(hardwareMap, Globals.DEFAULT_START_POSE, Globals.Side.BLUE, true);
-        controller = new PIDFController(P, I, D, F);
-        controller.setTolerance(Globals.SHOOTER_VELOCITY_TOLERANCE);
-        controller.setSetPoint(0);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        b = new com.pedropathing.control.PIDFController(new PIDFCoefficients(bp, 0, bd, bf));
+        s = new PIDFController(new PIDFCoefficients(sp, 0, sd, sf));
+    }
+
+    public double getTarget() {
+        return setPoint;
+    }
+    public double getVelocity() {
+        return robot.shooterSpinner2.getCorrectedVelocity();
     }
 
     @Override
     public void loop() {
-        controller.setP(P);
-        controller.setI(I);
-        controller.setI(D);
-        controller.setI(F);
+        b.setCoefficients(new PIDFCoefficients(bp, 0, bd, bf));
+        s.setCoefficients(new PIDFCoefficients(sp, 0, sd, sf));
 
-        controller.setSetPoint(setVelocity);
-        power = controller.calculate(robot.shooterSpinner2.getCorrectedVelocity(), setVelocity);
+        if (Math.abs(getTarget() - getVelocity()) < pSwitch) {
+            s.updateError(getTarget() - getVelocity());
+            robot.shooterSpinner1.set(s.run());
+            robot.shooterSpinner2.set(s.run());
+        } else {
+            b.updateError(getTarget() - getVelocity());
+            robot.shooterSpinner1.set(b.run());
+            robot.shooterSpinner2.set(b.run());
+        }
+
+        robot.clearCache();
+
+        if (hoodPosition < Globals.HOOD_LOWERED) hoodPosition = Globals.HOOD_LOWERED;
+        if (hoodPosition > Globals.HOOD_MAX) hoodPosition = Globals.HOOD_MAX;
 
         robot.hood.setPosition(hoodPosition);
-        robot.follower.update();
 
-        telemetry.addData("Limelight Distance from Blue Goal: ", robot.getGoalDistance());
-        telemetry.addData("Use this distance: ", robot.getDistanceToGoalPinpoint());
+        telemetry.addData("Distance: ", robot.getDistanceToGoalPinpoint());
         telemetry.addData("Current Velocity: ", robot.shooterSpinner2.getCorrectedVelocity());
         telemetry.addData("Hood Value: ", robot.hood.getPosition());
         telemetry.update();
 
         robot.clearCache();
+        robot.follower.update();
     }
 }
